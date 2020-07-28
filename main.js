c38b2a98a89179b9835e0b2e60f97496a823440e
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-28 15:27:36 -0400

    Finished Person constructor. Completed all methods.

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(newFirst, newLast, newAge, isMarried) {
+  return {
+    firstName: newFirst || "Anonymous",
+    lastName: newLast || "Person",
+    age: newAge,
+    married: isMarried || false,
+
+    goingOn: function() {
+      return this.age + 1;
+    },
+
+    ageUp: function() {
+      this.age++;
+    },
+    
+    getFullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+
+    marry: function(partner) {
+      this.married = true;
+      partner.married = true;
+      
+      this.spouseName = partner.getFullName();
+      partner.spouseName = this.getFullName();
+    },
+
+    divorce: function(partner) {
+      this.married = false;
+      partner.married = false;
+
+      delete this.spouseName;
+      delete partner.spouseName;
+    }
+  }
+}
 
 
 
